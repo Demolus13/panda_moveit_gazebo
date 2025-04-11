c402587a124725e1d2e4cfd779d4d0a309ca5966
Demolus13 <parthgovale@gmail.com>
2025-04-11 22:31:37 +0000

    Attaching and dettaching the object

--- src/multi_panda_demos/src/pick_place.cpp
+++ src/multi_panda_demos/src/pick_place.cpp
@@ -3,6 +3,7 @@
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <moveit_msgs/msg/collision_object.hpp>
+#include <moveit_msgs/msg/attached_collision_object.hpp>
 #include <shape_msgs/msg/solid_primitive.hpp>
 #include <rclcpp/executors/single_threaded_executor.hpp>
 #include <rclcpp/parameter_client.hpp>
@@ -16,163 +17,220 @@ using namespace std::chrono_literals;
 class PickPlaceController
 {
 public:
-  PickPlaceController(const rclcpp::Node::SharedPtr &node)
-    : node_(node),
-      move_group_(node, "dual_panda_arm"),
-      planning_scene_interface_()
-  {
-    move_group_.setMaxVelocityScalingFactor(0.25);
-    move_group_.startStateMonitor();
-  }
+    PickPlaceController(const rclcpp::Node::SharedPtr &node)
+        : node_(node),
+          move_group_(node, "dual_panda_arm"),
+          left_hand_group_(node, "left_panda_hand"),
+          planning_scene_interface_()
+    {
+        move_group_.setMaxVelocityScalingFactor(0.25);
+        move_group_.startStateMonitor();
+    }
 
-  void execute()
-  {
-    spawnObjects();  // 👈 Spawn SDF-based objects
+    void execute()
+    {
+        spawnObjects(); // Spawn objects in the planning scene
 
-    waitForValidState();
+        waitForValidState();
 
-    geometry_msgs::msg::PoseStamped pick_pose;
-    pick_pose.header.frame_id = "world";
-    pick_pose.pose.position.x = 0.4;
-    pick_pose.pose.position.y = 0.2;
-    pick_pose.pose.position.z = 0.4;
-    pick_pose.pose.orientation.w = 1.0;
+        // Open gripper
+        controlGripper(0.04); // open
+        std::this_thread::sleep_for(1s);
 
-    move_group_.setPoseTarget(pick_pose, "left_panda_link8");
-    planAndExecute("Pick pose");
+        // Move to pick pose
+        geometry_msgs::msg::PoseStamped pick_pose;
+        pick_pose.header.frame_id = "world";
+        pick_pose.pose.position.x = 0.35;
+        pick_pose.pose.position.y = -0.25;
+        pick_pose.pose.position.z = 0.4;
+        pick_pose.pose.orientation.w = 0.0;
 
-    std::this_thread::sleep_for(1s);
-    RCLCPP_INFO(node_->get_logger(), "Simulated pick action");
+        move_group_.setPoseTarget(pick_pose, "left_panda_link8");
+        planAndExecute("Pick pose");
 
-    geometry_msgs::msg::PoseStamped place_pose;
-    place_pose.header.frame_id = "world";
-    place_pose.pose.position.x = 0.6;
-    place_pose.pose.position.y = -0.2;
-    place_pose.pose.position.z = 0.4;
-    place_pose.pose.orientation.w = 1.0;
+        std::this_thread::sleep_for(1s);
 
-    move_group_.setPoseTarget(place_pose, "left_panda_link8");
-    planAndExecute("Place pose");
+        // Close gripper
+        controlGripper(0.015); // closed
+        std::this_thread::sleep_for(1s);
 
-    std::this_thread::sleep_for(1s);
-    RCLCPP_INFO(node_->get_logger(), "Simulated place action");
-  }
+        // Attach object
+        planning_scene_interface_.applyAttachedCollisionObject(
+            createAttachedObject("vertical_stick_1", "left_panda_leftfinger"));
+        RCLCPP_INFO(node_->get_logger(), "Object attached to gripper");
+
+        // Move to place pose
+        geometry_msgs::msg::PoseStamped place_pose;
+        place_pose.header.frame_id = "world";
+        place_pose.pose.position.x = 0.6;
+        place_pose.pose.position.y = -0.2;
+        place_pose.pose.position.z = 0.4;
+        place_pose.pose.orientation.w = 1.0;
+
+        move_group_.setPoseTarget(place_pose, "left_panda_link8");
+        planAndExecute("Place pose");
+
+        std::this_thread::sleep_for(1s);
+
+        // Detach object
+        planning_scene_interface_.applyAttachedCollisionObject(
+            createDetachedObject("vertical_stick_1"));
+        RCLCPP_INFO(node_->get_logger(), "Object detached from gripper");
+
+        std::this_thread::sleep_for(1s);
+
+        // Open gripper
+        controlGripper(0.04); // open
+    }
 
 private:
-  rclcpp::Node::SharedPtr node_;
-  moveit::planning_interface::MoveGroupInterface move_group_;
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+    rclcpp::Node::SharedPtr node_;
+    moveit::planning_interface::MoveGroupInterface move_group_;
+    moveit::planning_interface::MoveGroupInterface left_hand_group_;
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
 
-  void waitForValidState()
-  {
-    RCLCPP_INFO(node_->get_logger(), "Waiting for valid initial robot state...");
-    auto current_state = move_group_.getCurrentState(10.0);
-    if (!current_state)
+    void waitForValidState()
     {
-      throw std::runtime_error("Robot did not provide valid state within timeout");
+        RCLCPP_INFO(node_->get_logger(), "Waiting for valid initial robot state...");
+        auto current_state = move_group_.getCurrentState(10.0);
+        if (!current_state)
+        {
+            throw std::runtime_error("Robot did not provide valid state within timeout");
+        }
     }
-  }
 
-  void planAndExecute(const std::string &description)
-  {
-    moveit::planning_interface::MoveGroupInterface::Plan plan;
-    if (move_group_.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS)
+    void planAndExecute(const std::string &description)
     {
-      RCLCPP_INFO(node_->get_logger(), "%s: Planning successful, executing...", description.c_str());
-      auto result = move_group_.execute(plan);
-      if (result != moveit::core::MoveItErrorCode::SUCCESS)
-      {
-        throw std::runtime_error(description + " execution failed.");
-      }
-      RCLCPP_INFO(node_->get_logger(), "%s: Execution complete", description.c_str());
+        moveit::planning_interface::MoveGroupInterface::Plan plan;
+        if (move_group_.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS)
+        {
+            RCLCPP_INFO(node_->get_logger(), "%s: Planning successful, executing...", description.c_str());
+            auto result = move_group_.execute(plan);
+            if (result != moveit::core::MoveItErrorCode::SUCCESS)
+            {
+                throw std::runtime_error(description + " execution failed.");
+            }
+            RCLCPP_INFO(node_->get_logger(), "%s: Execution complete", description.c_str());
+        }
+        else
+        {
+            throw std::runtime_error(description + " planning failed.");
+        }
     }
-    else
+
+    void controlGripper(double position)
     {
-      throw std::runtime_error(description + " planning failed.");
+        std::map<std::string, double> target;
+        target["left_panda_finger_joint1"] = position;
+        target["left_panda_finger_joint2"] = position;
+        left_hand_group_.setJointValueTarget(target);
+        left_hand_group_.move();
+        RCLCPP_INFO(node_->get_logger(), "Gripper moved to position %.3f", position);
     }
-  }
 
-  void spawnObjects()
-  {
-    std::vector<moveit_msgs::msg::CollisionObject> collision_objects;
+    moveit_msgs::msg::AttachedCollisionObject createAttachedObject(const std::string &object_id, const std::string &link_name)
+    {
+        moveit_msgs::msg::AttachedCollisionObject attached_object;
+        attached_object.link_name = link_name;
+        attached_object.object.id = object_id;
+        attached_object.object.operation = attached_object.object.ADD;
 
-    // Vertical Stick 1
-    moveit_msgs::msg::CollisionObject stick1;
-    stick1.header.frame_id = "world";
-    stick1.id = "vertical_stick_1";
+        // Specify touch links (fingers allowed to touch object)
+        attached_object.touch_links = {"left_panda_leftfinger", "left_panda_rightfinger"};
 
-    shape_msgs::msg::SolidPrimitive stick_shape;
-    stick_shape.type = stick_shape.BOX;
-    stick_shape.dimensions = {0.02, 0.02, 0.2};
+        return attached_object;
+    }
 
-    geometry_msgs::msg::Pose stick1_pose;
-    stick1_pose.position.x = 0.35;
-    stick1_pose.position.y = -0.25;
-    stick1_pose.position.z = 0.1;
-    stick1_pose.orientation.w = 1.0;
+    moveit_msgs::msg::AttachedCollisionObject createDetachedObject(const std::string &object_id)
+    {
+        moveit_msgs::msg::AttachedCollisionObject detached_object;
+        detached_object.object.id = object_id;
+        detached_object.object.operation = detached_object.object.REMOVE;
+        return detached_object;
+    }
 
-    stick1.primitives.push_back(stick_shape);
-    stick1.primitive_poses.push_back(stick1_pose);
-    stick1.operation = stick1.ADD;
-    collision_objects.push_back(stick1);
+    void spawnObjects()
+    {
+        std::vector<moveit_msgs::msg::CollisionObject> collision_objects;
 
-    // Vertical Stick 2
-    moveit_msgs::msg::CollisionObject stick2 = stick1;
-    stick2.id = "vertical_stick_2";
-    stick2.primitive_poses[0].position.y = 0.25;
-    collision_objects.push_back(stick2);
+        // Vertical Stick 1
+        moveit_msgs::msg::CollisionObject stick1;
+        stick1.header.frame_id = "world";
+        stick1.id = "vertical_stick_1";
 
-    planning_scene_interface_.applyCollisionObjects(collision_objects);
-    RCLCPP_INFO(node_->get_logger(), "Spawned SDF-based collision objects.");
-  }
+        shape_msgs::msg::SolidPrimitive stick_shape;
+        stick_shape.type = stick_shape.BOX;
+        stick_shape.dimensions = {0.02, 0.02, 0.2};
+
+        geometry_msgs::msg::Pose stick1_pose;
+        stick1_pose.position.x = 0.35;
+        stick1_pose.position.y = -0.25;
+        stick1_pose.position.z = 0.1;
+        stick1_pose.orientation.w = 1.0;
+
+        stick1.primitives.push_back(stick_shape);
+        stick1.primitive_poses.push_back(stick1_pose);
+        stick1.operation = stick1.ADD;
+        collision_objects.push_back(stick1);
+
+        // Vertical Stick 2
+        moveit_msgs::msg::CollisionObject stick2 = stick1;
+        stick2.id = "vertical_stick_2";
+        stick2.primitive_poses[0].position.y = 0.25;
+        collision_objects.push_back(stick2);
+
+        planning_scene_interface_.applyCollisionObjects(collision_objects);
+        RCLCPP_INFO(node_->get_logger(), "Spawned SDF-based collision objects.");
+    }
 };
 
 int main(int argc, char **argv)
 {
-  try
-  {
-    rclcpp::init(argc, argv);
-
-    rclcpp::NodeOptions options = rclcpp::NodeOptions().allow_undeclared_parameters(true);
-    auto node = std::make_shared<rclcpp::Node>("pick_place_node", options);
-
-    auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(node, "/move_group");
-    while (!parameters_client->wait_for_service(1s))
+    try
     {
-      if (!rclcpp::ok())
-      {
-        RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for parameter service.");
-        return 1;
-      }
-      RCLCPP_INFO(node->get_logger(), "Waiting for /move_group parameter service...");
+        rclcpp::init(argc, argv);
+
+        rclcpp::NodeOptions options = rclcpp::NodeOptions().allow_undeclared_parameters(true);
+        auto node = std::make_shared<rclcpp::Node>("pick_place_node", options);
+
+        auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(node, "/move_group");
+        while (!parameters_client->wait_for_service(1s))
+        {
+            if (!rclcpp::ok())
+            {
+                RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for parameter service.");
+                return 1;
+            }
+            RCLCPP_INFO(node->get_logger(), "Waiting for /move_group parameter service...");
+        }
+
+        auto parameter_list = parameters_client->list_parameters({"robot_description_kinematics"}, 10);
+        auto parameters = parameters_client->get_parameters(parameter_list.names);
+        node->set_parameters(parameters);
+
+        auto executor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+        executor->add_node(node);
+        std::thread executor_thread([&executor]()
+                                    { executor->spin(); });
+
+        PickPlaceController controller(node);
+        controller.execute();
+
+        executor->cancel();
+        executor_thread.join();
+        rclcpp::shutdown();
+        return 0;
     }
-
-    auto parameter_list = parameters_client->list_parameters({"robot_description_kinematics"}, 10);
-    auto parameters = parameters_client->get_parameters(parameter_list.names);
-    node->set_parameters(parameters);
-
-    auto executor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-    executor->add_node(node);
-    std::thread executor_thread([&executor]() { executor->spin(); });
-
-    PickPlaceController controller(node);
-    controller.execute();
-
-    executor->cancel();
-    executor_thread.join();
-    rclcpp::shutdown();
-    return 0;
-  }
-  catch (const std::exception &e)
-  {
-    std::cerr << "Fatal error: " << e.what() << std::endl;
-    rclcpp::shutdown();
-    return 1;
-  }
-  catch (...)
-  {
-    std::cerr << "Unknown fatal error occurred." << std::endl;
-    rclcpp::shutdown();
-    return 1;
-  }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Fatal error: " << e.what() << std::endl;
+        rclcpp::shutdown();
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown fatal error occurred." << std::endl;
+        rclcpp::shutdown();
+        return 1;
+    }
 }
